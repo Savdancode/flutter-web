c6dac9de3cfd1139484a70dfa5cc3dc51adbf5e7
Savdan <soun.savdanit@gmail.com>
2024-02-19 17:12:21 +0700

    Change follow structure

--- lib/widgets/home/home_body.dart
+++ lib/widgets/home/home_body.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:web_shop/widgets/home/zoom_in_hover.dart';
+import 'package:web_shop/widgets/products/product_cart.dart';
 
 class HomeBody extends StatefulWidget {
   final double maxWidth;
@@ -183,52 +183,7 @@ class _HomeBodyState extends State<HomeBody> {
                         mainAxisSpacing: 15),
                     itemCount: 9,
                     itemBuilder: (context, i) {
-                      return ZoomInOnHover(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
-                            color: Colors.grey[200],
-                          ),
-                          child: Column(
-                            children: [
-                              Container(
-                                alignment: Alignment.bottomLeft,
-                                height: 300,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(12),
-                                  image: const DecorationImage(
-                                    image: AssetImage('assets/husky.jpeg'),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                child: Container(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 10, vertical: 5),
-                                  color: Colors.amber,
-                                  child: const Text(
-                                    '245 \$',
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              const Flexible(
-                                child: Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-                                    overflow: TextOverflow.ellipsis,
-                                    textAlign: TextAlign.justify,
-                                    maxLines: 8,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
+                      return ProductCart();
                     }),
               ],
             ),
---
+++ lib/widgets/products/product_cart.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class ProductCart extends StatefulWidget {
+  const ProductCart({
+    super.key,
+  });
+
+  @override
+  _ProductCartState createState() => _ProductCartState();
+}
+
+class _ProductCartState extends State<ProductCart> {
+  bool _isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.easeInOut,
+        transform: _isHovered ? Matrix4.skewY(0.0) : null,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.grey[200],
+          ),
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.bottomLeft,
+                height: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/husky.jpeg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  color: Colors.amber,
+                  child: const Text(
+                    '245 \$',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const Flexible(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.justify,
+                    maxLines: 8,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
